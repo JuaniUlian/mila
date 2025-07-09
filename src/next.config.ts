a4f5e7f49bc3fc70bd71bf4bd98449eea52bde31
juaniulian <juanulian@gmail.com>
2025-07-09 15:38:10 +0000

    ahora que?

---
+++ src/next.config.ts
@@ -0,0 +1,28 @@
+import type {NextConfig} from 'next';
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+  experimental: {
+    allowedDevOrigins: [
+      "https://9003-firebase-studio-1747512173228.cluster-duylic2g3fbzerqpzxxbw6helm.cloudworkstations.dev"
+    ]
+  }
+};
+
+export default nextConfig;
